0a7a73f47a6312f0a45b184adc87be3e1be5dde0
RonhoGroniko <sarapovrodion@gmail.com>
2025-08-25 15:49:07 +0200

    Inject viewModels

--- app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/MainActivity.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/MainActivity.kt
@@ -3,27 +3,32 @@ package com.example.dependencyinjectionstart.example2.presentation
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.example.dependencyinjectionstart.R
 import com.example.dependencyinjectionstart.example2.ExampleApp
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
-//
-//    @Inject
-//    lateinit var viewModel: ExampleViewModel
+
+    @Inject
+    lateinit var viewModelFactory: ViewModelFactory
+
+    private val viewModel by lazy {
+        ViewModelProvider(this, viewModelFactory)[ExampleViewModel::class.java]
+    }
 
     private val component by lazy {
         (application as ExampleApp).component
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-//        component.inject(this)
+        component.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d("MainActivity", "${component.getDatabase()}")
         Log.d("MainActivity", "${component.getDatabase()}")
         Log.d("MainActivity", "${component.getApiService()}")
         Log.d("MainActivity", "${component.getApiService()}")
-//        viewModel.method()
+        viewModel.method()
     }
 }
