fb0637f90252fe756dc2704c3d4b6e049f8b36d0
RonhoGroniko <sarapovrodion@gmail.com>
2025-08-25 18:25:32 +0200

    Add qualifiers

---
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/data/datasource/ExampleTestRemoteDataSourceImpl.kt
@@ -0,0 +1,12 @@
+package com.example.dependencyinjectionstart.example2.data.datasource
+
+import android.util.Log
+import com.example.dependencyinjectionstart.example2.data.network.ExampleApiService
+import javax.inject.Inject
+
+class ExampleTestRemoteDataSourceImpl @Inject constructor() : ExampleRemoteDataSource {
+
+    override fun method() {
+        Log.d("EXAMPLE_TEST", "Test")
+    }
+}
